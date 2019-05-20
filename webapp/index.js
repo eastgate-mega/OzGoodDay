9c97eb5f34e7917d836230a8009e8b46115fd036
Jimmy <mrxujimmy@gmail.com>
2019-05-20 11:05:35 +1000

    add comment

--- webapp/index.js
+++ webapp/index.js
@@ -1,13 +1,16 @@
+// import express
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+// contactpage
 app.get('/', function(req, res){
   res.render('ContactPage.ejs');
 });
 
+// start server here
 app.listen(3000, 'localhost', function(){
   console.log('Server start!');
 });
