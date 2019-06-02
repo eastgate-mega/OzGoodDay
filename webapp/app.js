cfc144e5a8bcd893af04ed67d8b7a495377ed6a8
Jimmy <mrxujimmy@gmail.com>
2019-06-02 22:23:33 +1000

    merge server.js to app.js, no funciton changes, need associate user with secret page next step

--- webapp/app.js
+++ webapp/app.js
@@ -1,8 +1,15 @@
-// import express
-var express = require('express'),
-    app = express();
-    mysql = require('mysql');
+// import package
+var express     = require("express"),
+    app         = express(),
+    mysql       = require('mysql'),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User        = require("./models/user");
 
+
+// MySQL CONFIG
 var db = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -18,8 +25,23 @@ db.connect(function(err) {
   console.log('connected as id ' + db.threadId);
 });
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+// MONGOOSE CONFIG
+mongoose.connect("mongodb://localhost/webapp", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // // create a database here
 app.get('/createDB', function(req, res){
@@ -130,16 +152,63 @@ app.get('/product', function(req, res){
   res.render('productPage')
 });
 
-app.get('/customer/account/login', function(req, res){
-  res.render('customer_login');
+
+
+
+// ==============
+// AUTH ROUTE 
+// ==============
+// show register form
+app.get('/register', function(req, res){
+  res.render('login/register');
+});
+
+// show login form
+app.get("/login", function(req, res){
+  res.render("login/login"); 
+});
+
+//handling user sign up
+app.post("/register", function(req, res){
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+      if(err){
+          console.log(err);
+          res.send(err.message);
+          return res.render('login/register');
+      }
+      passport.authenticate("local")(req, res, function(){
+         res.redirect("/secret");
+      });
+  });
+});
+
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/secret",
+  failureRedirect: "/login"
+}) ,function(req, res){
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+// secret page for user only
+app.get("/secret",isLoggedIn, function(req, res){
+  
+  res.render("login/secret");
 });
 
 
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+      return next();
+  }
+  res.redirect("/login");
+}
 
-// login and sign up function goes here
-app.get('/customer/account/new', function(req, res){
-  res.render('customer_new');
-});
 
 // start server here
 app.listen(3000, 'localhost', function(){
