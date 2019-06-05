7415935106fc077b93ea8c7f80efd564f603dd09
Jimmy <mrxujimmy@gmail.com>
2019-06-05 13:06:28 +1000

    re-build the database(item schema)

--- webapp/models/item.js
+++ webapp/models/item.js
@@ -2,11 +2,32 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var itemSchema = new Schema({
-    name: String,
-    price: Number,
-    description: String
+    product_name: {type: String, isRequired: true},
+    supply_item_number: {type: String, isRequired:true},
+    category: {type: String, isRequired: true},
+    barCode: {type: Number, isRequired: true, default: 0},
+    image_path:[
+        {
+            type: String,
+            isRequired: false,
+            default: 'https://thumbs.dreamstime.com/t/demo-sign-icon-stamp-blue-vector-92101308.jpg'
+        }
+    ],
+    create_data: {type: Date, default: Date.now},
+    last_modified: {type: Date, default: Date.now},
+    brand: {type: String, default: 'None'},
+    cost: {type: Number, isRequired: true},
+    original_price: {type: Number, isRequired: true},
+    quantity: {type: Number, isRequired: true},
+    //need to be changed to enum type in the furture
+    color: {type: String, default: 'black'},
+    sold_amount: {type: Number, default: 0},
+    // unit of measurement is set to be 'g'
+    weight: {type: Number, default: 1000},
+    return_status: {type: Boolean, default: false}
 
-})
+
+});
 
 
 module.exports = mongoose.model('Item', itemSchema);
