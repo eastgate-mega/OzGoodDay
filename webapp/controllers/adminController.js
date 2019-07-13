70d92b63e2382cc37139abf90b1bbc09effd9554
Jimmy <mrxujimmy@gmail.com>
2019-07-13 17:36:01 +1000

    refact admin router

--- webapp/controllers/adminController.js
+++ webapp/controllers/adminController.js
@@ -1,59 +1,75 @@
+'use strict';
+
 const User = require('../models/user'),
 passport = require('passport'),
 Address = require('../models/postAddress')
 
-exports.customer_profile_get = (req, res) => {
-    res.render('login/user_profile');
-}
+class Admin{
+    constructor(){
 
-exports.customer_register_get = (req, res) => {
-    res.render('login/register');
-}
+    }
 
-exports.customer_register_post = (req, res) => {
-    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            res.send(err.message);
-            return res.render('login/register');
-        }
-        passport.authenticate("local")(req, res, function(){
-            res.redirect("/");
+    async customer_profile_get(req, res) {
+        res.render('login/user_profile');
+        return
+    }
+
+    async customer_login_get(req, res) {
+        res.render("login/login");
+        return
+    }
+
+    async customer_register_get(req, res) {
+        res.render('login/register');
+    }
+
+    async customer_register_post(req, res) {
+        User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+            if(err){
+                console.log(err);
+                res.send(err.message);
+                return res.render('login/register');
+            }
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/");
+            });
         });
-    });
+    }
+
+    async customer_login_get(req, res) {
+        res.render("login/login");
+    }
+
+    async customer_login_post(req, res) {
+        passport.authenticate("local", {
+            successRedirect: "/",
+            failureRedirect: "/login"
+            })
+    }
+
+    async customer_logout(req, res) {
+        req.logout();
+        res.redirect("/");
+    }
+
+    async customer_address_get(req, res) {
+        Address.find(function(err, address){
+            if (err) {
+                console.log(err);
+                
+            } else {
+                res.send(address);
+            }
+        })
+    }
+
+    async customer_address_post(req, res) {
+        res.send('address has been create');
+    }
+
+    async customer_address_put(req, res) {
+        res.send('address has been update');
+    }
 }
 
-exports.customer_login_get = (req, res) => {
-  res.render("login/login");
-}
-
-exports.customer_logout = (req, res) => {
-  req.logout();
-  res.redirect("/");
-}
-
-exports.customer_login_post = (req, res) => {
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login"
-    })
-}
-
-exports.customer_address_get = (req, res) => {
-    Address.find(function(err, address){
-        if (err) {
-            console.log(err);
-            
-        } else {
-            res.send(address);
-        }
-    })
-}
-
-exports.customer_address_post = (req, res) => {
-    res.send('address has been create');
-}
-
-exports.customer_address_put = (req, res) =>{
-    res.send('address has been update');
-}
+module.exports = new Admin();
--- webapp/routes/admin.js
+++ webapp/routes/admin.js
@@ -4,26 +4,18 @@ passport = require('passport'),
 Admin = require('../controllers/adminController')
 
 router.get('/:id/profile', isLoggedIn, Admin.customer_profile_get);
-
 router.get('/register', Admin.customer_register_get);
-
 router.post("/register", Admin.customer_register_post);
-
 router.get("/login", Admin.customer_login_get);
-
 router.post("/login", passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login"
 }));
-
-
 router.get("/logout", Admin.customer_logout);
 
 //user address detail API
 router.get('/address', Admin.customer_address_get);
-
 router.post('/address', Admin.customer_address_post);
-
 router.put('/address', Admin.customer_address_put);
 
 function isLoggedIn(req, res, next) {
